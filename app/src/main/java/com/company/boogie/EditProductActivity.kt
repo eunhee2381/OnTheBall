9e556ab67fbc56bed99332584b4fd948e31cfe09
suakim <fiigjam@hansung.ac.kr>
2024-03-30 12:29:06 +0900

    로그인/로그아웃/회원가입/기자재 정보수정 activity 생성

---
+++ app/src/main/java/com/company/boogie/EditProductActivity.kt
@@ -0,0 +1,14 @@
+package com.company.boogie
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditProductActivity {
+    class EditProductActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+            super.onCreate(savedInstanceState, persistentState)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/company/boogie/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.company.boogie
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity {
+    class LoginActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+            super.onCreate(savedInstanceState, persistentState)
+
+        }
+    }
+}
