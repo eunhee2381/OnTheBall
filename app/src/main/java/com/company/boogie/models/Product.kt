137396bdce5c902c892a30ac9c90a4ef6dda727a
xoals <ptm129@naver.com>
2024-04-01 17:46:54 +0900

    Product dataclass추가

---
+++ app/src/main/java/com/company/boogie/models/Product.kt
@@ -0,0 +1,10 @@
+package com.company.boogie.models
+
+
+data class Product(
+    val name: String = "",
+    val img: String = "",
+    val detail: String = "",
+    val location: String = "",
+    val classificationCode: Int = 0
+)
