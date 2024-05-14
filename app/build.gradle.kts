95ad89e3f733955535092c55630e95e3988acd82
xoals <ptm129@naver.com>
2024-05-14 14:08:15 +0900

    FirebaseRequestUtil, FirestoreMessagingModel, FirestoreProductModel 파일 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth-ktx")
-
-
+    implementation (libs.firebase.messaging)
 }
