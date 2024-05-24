a9041f74bac3ba077d8d7402e10e9159d411b203
clickclick-ctrl <clickclick-ctrl@users.noreply.github.com>
2024-05-25 06:17:45 +0900

    코드 수정: User class 속성

--- app/src/main/java/com/company/boogie/models/User.kt
+++ app/src/main/java/com/company/boogie/models/User.kt
@@ -4,10 +4,10 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.PropertyName
 
 data class User(
-    @get:PropertyName("email") @set:PropertyName("email") var email: String = "",
-    @get:PropertyName("name") @set:PropertyName("name") var name: String = "",
-    @get:PropertyName("birthday") @set:PropertyName("birthday") var birthday: Timestamp = Timestamp.now(),
-    @get:PropertyName("studentID") @set:PropertyName("studentID") var studentID: Int = 0,
+    @PropertyName("email") val email: String = "",
+    @PropertyName("name") val name: String = "",
+    @PropertyName("birthday") val birthday: Timestamp = Timestamp.now(),
+    @PropertyName("studentID") val studentID: Int = 0,
     @get:PropertyName("isAdmin") @set:PropertyName("isAdmin") var isAdmin: Boolean = false,
     @get:PropertyName("isBanned") @set:PropertyName("isBanned") var isBanned: Boolean = false,
     @get:PropertyName("borrowing") @set:PropertyName("borrowing") var borrowing: String = "",
