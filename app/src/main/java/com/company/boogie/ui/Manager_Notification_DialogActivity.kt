072b1545e91d2b5142ac817b72b86d21a8da34c8
junseoung <ehcl34@naver.com>
2024-05-25 12:21:56 +0900

    알람 다이얼로그

---
+++ app/src/main/java/com/company/boogie/ui/Manager_Notification_DialogActivity.kt
@@ -0,0 +1,32 @@
+package com.company.boogie.ui
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.company.boogie.R
+
+class Manager_Notification_DialogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE) // 타이틀 바 제거
+        setContentView(R.layout.manager_notification_dialog)
+
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        window.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val btnAccept: Button = findViewById(R.id.btn_accept)
+        val btnRefuse: Button = findViewById(R.id.btn_refuse)
+
+        btnAccept.setOnClickListener {
+            // 수락동작
+            finish() // 다이얼로그 닫기
+        }
+
+        btnRefuse.setOnClickListener {
+            //거절동작
+            finish() // 다이얼로그 닫기
+        }
+    }
+}
